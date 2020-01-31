a4374cd0f47fa8304a7c55a560ecae90324d5d84
laboratoria <b.floriano@outlook.com>
2020-01-31 19:09:43 -0300

    heroku deploy

--- config/config.js
+++ config/config.js
@@ -1,4 +1,6 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
     "username": "postgres",
     "password": null,
@@ -16,11 +18,14 @@
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
-    "operatorsAliases": false
-  }
+    "operatorsAliases": false,
+    "dialectOptions": {
+      "ssl": true
+    }
+}
 }
